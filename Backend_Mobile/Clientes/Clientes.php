0ac53e33c928d7bf674d93df669e78e8d5a9d528
guero10delgado <60528507+guero10delgado@users.noreply.github.com>
2021-04-23 23:10:22 -0500

    Select e insert

--- Backend_Mobile/Clientes/Clientes.php
+++ Backend_Mobile/Clientes/Clientes.php
@@ -30,6 +30,20 @@ class Clientes {
       }
    }
 
+   public static function getClients() {
+
+      $consulta = "SELECT * FROM TCLIENTES";
+      try {
+          $comando = Database::getInstance() -> getDb() -> prepare($consulta);
+          $comando -> execute();
+
+          return $comando -> fetchAll(PDO::FETCH_ASSOC);
+
+      } catch (PDOException $e) {
+          return false;
+      }
+   }
+
 
 }
 
